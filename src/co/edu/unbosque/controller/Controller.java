1740f4c7e87bc82d9707cacbcac1d701f3ecc00e
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-12-03 14:51:26 -0500

    Laberinto de la liebre

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -4,6 +4,7 @@
 package co.edu.unbosque.controller;
 
 import co.edu.unbosque.model.Modelo;
+import co.edu.unbosque.model.ExceptionNumero;
 import co.edu.unbosque.view.Vista;
 
 /**
@@ -28,29 +29,66 @@ public class Controller {
 		String opcion = "";
 		String seguir = "";
 		do {
-			opcion = vista.leerDato(
-					"Ingrese la opción que desea realizar\n1.Nutricionista\n2.Liga de fútbol\n3.Saltos de conejo");
-			switch (opcion) {
-			case "1":
-				vista.imprimirDato("Opcion1");
-				break;
-			case "2":
-				vista.imprimirDato("Opcion2");
-				break;
-			case "3":
-				vista.imprimirDato("Opcion4");
-				break;
-			default:
-				vista.imprimirDato("Default");
-				break;
+			try {
+				opcion = vista.leerDato(
+						"Ingrese la opción que desea realizar\n1.Nutricionista\n2.Liga de fútbol\n3.Saltos de conejo");
+				switch (opcion) {
+				case "1":
+					vista.imprimirDato("Opcion1");
+					break;
+				case "2":
+					vista.imprimirDato("Opcion2");
+					break;
+				case "3":
+					String numeroFilas = "";
+					String numeroColumnas = "";
+					String inicioX = "";
+					String inicioY = "";
+					String finX = "";
+					String finY = "";
+					String caminosP = "";
+					String caminosQ = "";
+					numeroFilas = vista.leerDato("Ingrese el número de filas del campo");
+					modelo.verificarNumero(numeroFilas);
+					numeroColumnas = vista.leerDato("Ingrese el número de columnas del campo");
+					modelo.verificarNumero(numeroColumnas);
+					inicioX = vista.leerDato("Ingrese la cordenada x del punto de partia de la liebre");
+					modelo.verificarNumero(inicioX);
+					inicioY = vista.leerDato("Ingrese la cordenada y del punto de partia de la liebre");
+					modelo.verificarNumero(inicioY);
+					finX = vista.leerDato("Ingrese la cordenada x del punto de llegada de la liebre");
+					modelo.verificarNumero(finX);
+					finY = vista.leerDato("Ingrese la cordenada y del punto de llegada de la liebre");
+					modelo.verificarNumero(finY);
+					caminosP = vista.leerDato("Ingrese el numero de caminos P");
+					modelo.verificarNumero(caminosP);
+					caminosQ = vista.leerDato("Ingrese el numero de caminos Q");
+					modelo.verificarNumero(caminosQ);
+					modelo.buscarSaltosLiebre(Integer.parseInt(numeroFilas), Integer.parseInt(numeroColumnas),
+							Integer.parseInt(inicioX), Integer.parseInt(inicioY), Integer.parseInt(finX),
+							Integer.parseInt(finY), Integer.parseInt(caminosP), Integer.parseInt(caminosQ));
+
+					break;
+				default:
+					vista.imprimirDato("Opción no valida");
+					break;
+				}
+			} catch (ExceptionNumero e) {
+				vista.imprimirDato(e.getMessage());
 			}
 			seguir = vista.leerDato("Desea seguir (S/N)");
 			seguir.toUpperCase();
-			while (!seguir.equals("N") || !seguir.equals("S")) {
-				seguir = vista.leerDato("Ingrese un dato valido (S/N)");
-			}
+			int k = 0;
+			do {
+				if (seguir.equals("n") || seguir.equals("s")) {
+					k = 0;
+				} else {
+					k = 1;
+					seguir = vista.leerDato("Ingrese un dato valido (S/N)");
+				}
+			} while (k == 1);
 
-		} while (seguir.equals("S"));
+		} while (seguir.equals("s"));
 
 	}
 }
--- src/co/edu/unbosque/model/LaberintoLiebre.java
+++ src/co/edu/unbosque/model/LaberintoLiebre.java
@@ -2,41 +2,20 @@ package co.edu.unbosque.model;
 
 import java.util.ArrayList;
 
-public class Ejercicio_recursividad_DDR_22 {
-
-	public static void main(String[] args) {
-
-		Casilla[][] tablero = new Casilla[6][6];
-
-		for (int i = 1; i < (tablero.length - 1); i++) {
-			for (int j = 1; j < (tablero.length - 1); j++) {
-				tablero[i][j] = new Casilla(i,j);
-			}
-		}
-		tablero[4][4].setFin(true);
-
-		ArrayList<Casilla> camino = new ArrayList<>();
-		int p = 3;
-		int q = 3;
-		int anterior = 0;
-
-		Laberinto laberinto = new Laberinto(tablero);
-		camino.add(tablero[1][2]);
-
-		rellenarCaminosV2(laberinto, tablero[1][2], camino, p, q, anterior, "q");
-
-		laberinto.mostrarCaminos();
-
+public class LaberintoLiebre {
+	
+	public void LaberintoLiebre() {
+		
 	}
 
-	public static void rellenarCaminosV2(Laberinto laberinto, Casilla casillaActual, ArrayList<Casilla> camino, int p,
+	public void rellenarCaminosV2(Laberinto laberinto, Casilla casillaActual, ArrayList<Casilla> camino, int p,
 			int q, int anterior, String saber) {
 
 		if (casillaActual.isFin()) {
-			
-			if(camino.size() <= laberinto.getCaminos().size() && laberinto.getCaminos().size() != 0) {
+
+			if (camino.size() <= laberinto.getCaminos().size() && laberinto.getCaminos().size() != 0) {
 				laberinto.añadirCamino((ArrayList<Casilla>) camino.clone());
-			}else {
+			} else {
 				laberinto.añadirCamino((ArrayList<Casilla>) camino.clone());
 			}
 
@@ -66,7 +45,7 @@ public class Ejercicio_recursividad_DDR_22 {
 		}
 	}
 
-	public static void caminos(Laberinto laberinto, Casilla casillaActual, ArrayList<Casilla> camino, int p, int q,
+	public void caminos(Laberinto laberinto, Casilla casillaActual, ArrayList<Casilla> camino, int p, int q,
 			int anterior, String saber, int[][] movimientos) {
 
 		int posXnueva, posYnueva;
